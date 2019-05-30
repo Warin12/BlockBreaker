29f160c1fe11a9db0aa4129da4126b0db18da596
Warin12 <evanw8@learn.cssd.ab.ca>
2019-05-30 15:06:50 -0600

    section3

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -7,13 +7,18 @@ public class Ball : MonoBehaviour
     [SerializeField] Paddle paddle1;
     [SerializeField] float xPush = 2f;
     [SerializeField] float yPush = 15f;
+    [SerializeField] AudioClip[] ballSounds;
     Vector2 paddleToBallVector;
     bool hasStarted = false;
+
+    //cached component references
+    AudioSource myAudioSource;
     
     
     void Start()
     {
         paddleToBallVector = transform.position - paddle1.transform.position;
+        myAudioSource = GetComponent<AudioSource>();
     }
     void Update()
     {
@@ -39,5 +44,16 @@ public class Ball : MonoBehaviour
     {
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y );
         transform.position = paddlePos + paddleToBallVector;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (hasStarted)
+        {
+        AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];    
+        myAudioSource.PlayOneShot(clip);
+        
+        
+        }
     }
 }
