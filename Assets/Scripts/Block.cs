56d03a8eb81ad800ac1212bb085676d52c6c257e
Warin12 <evanw8@learn.cssd.ab.ca>
2019-06-05 15:10:45 -0600

    Tagsinunity

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
+    [SerializeField] GameObject blockSparklesVFX;
     
     Level level;
 
@@ -21,10 +22,22 @@ public class Block : MonoBehaviour
 
     private void DestroyBlock()
     {
-       FindObjectOfType<GameStatus>().AddToScore();
-        AudioSource.PlayClipAtPoint(breakSound,Camera.main.transform.position);
+        PlayBlockDestroySFX();
         Destroy(gameObject);
         level.BlockDestroyed();
+        TriggerSparklesVFX();
 
     }
+
+    private void PlayBlockDestroySFX()
+    {
+        FindObjectOfType<GameSession>().AddToScore();
+        AudioSource.PlayClipAtPoint(breakSound,Camera.main.transform.position);
+    }
+    private void TriggerSparklesVFX()
+    {
+        GameObject sparkles = Instantiate(blockSparklesVFX, transform.position, transform.rotation);
+        Destroy(sparkles, 1f);
+    }
 }
+ 
